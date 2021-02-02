1d33221ccaa5c2fa363a233090805a60c9a2d32f
sa <sm.anas@outlook.com>
2021-02-02 23:43:20 +0530

    Generate User and Following model
    Generate following model to implement the fucntionality of following a
    user.
    
    Developed a relationship between following and user model. A user can
    have many users_they_followed through following model which corresponds
    to follower_id in following model. Similarly, user has many
    following_users which corresponds to followed_id in following table

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :fullname, presence: true
+
+  has_many :users_they_followed, class_name: "Following", foreign_key: :follower_id
+  has_many :followings, through: :users_they_followed, source: :followed
+
+  has_many :following_users, class_name: "Following", foreign_key: :followed_id
+  has_many :followers, through: :following_users
+
+end
