38ca9ca61ae305f05e498690d7702604368e9be5
sa <sm.anas@outlook.com>
2021-02-02 23:54:09 +0530

    Add methods for login checks in application_controller
    Added helper methods for Session Functionality in application helper
    Added to method to check if a user is logged_in?. It check if the
    currentcurrent_user_id is nil
    Added method to require_login which redirect_to to login path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  def logged_in?
+    session[:current_user_id] != nil
+  end
+
+  def require_login!
+    return if logged_in?
+
+    flash[:error] = 'You must be logged in'
+    redirect_to login_path
+  end
+
+
+  def current_user
+    @_current_user ||= session[:current_user_id] &&
+      User.find_by(id: session[:current_user_id])
+  end  
+ 
+
 end
