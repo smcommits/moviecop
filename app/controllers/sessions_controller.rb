9a59bfcf021723278e6c5f232b6e92c53a9b1f19
sa <sm.anas@outlook.com>
2021-02-02 23:52:25 +0530

    Add SessionsController
    Addes sessions controller to handle login and user authentication.
    Created the methods; new and create within SessioinsController. Create
    check if the user is present using username and creates a
    sessions[current_user_id]

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+  skip_before_action :require_login!, only: [:new, :create]
+
+  def new
+  end
+  def create
+    if user = User.find_by_username(params[:username])
+      session[:current_user_id] = user.id
+      redirect_to root_path
+    else
+      flash[:alert] = 'Username Not Found!'
+      render :new
+    end
+  end
+
+  def destroy
+    session.delete(:current_user_id)
+    @_current_user = nil
+    redirect_to login_path
+  end
+
+
+end
