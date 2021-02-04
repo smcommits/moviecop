1187efa75ec1b8bc6650822fa9cd994c1783681a
sa <sm.anas@outlook.com>
2021-02-04 20:07:29 +0530

    Add unit testing for User model method
    Added tests for instance methods in user model
    Test for the methods:
    - following_count

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
+  let(:user) {User.create(username: "Test", fullname:"Tester")}
+  let(:user2) {User.create(username: "Test2", fullname:"Tester2")}
+  let(:example_user) {User.create(username: "Ex", fullname: "Example")}
+
   context 'validations' do
     it { should validate_presence_of(:username) }
     it { should validate_uniqueness_of(:username) }
@@ -16,6 +20,21 @@ RSpec.describe User, type: :model do
     it { should have_many(:followers)}
     it { should have_many(:opinions) }
   end
+
+  context 'instance methods' do
+    
+    describe "#following_count" do
+      it "should return the count of users that a user is following" do
+        count = user.following_count 
+        expect(count).to eql(0)
+      end
+
+      it "should return the count of following users" do
+        Following.create(follower_id: user.id, followed_id: user2.id)
+        count = user.following_count
+        expect(count).to eql(1)
+      end
+    end
 
 end
 
